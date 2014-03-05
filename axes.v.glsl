d7774fa64ef477a386ed59e34a693fbb61c85e20
Karol Marcjan <karol.marcjan@gmail.com>
2014-03-05 14:08:19 +0100

    Add Axes shader sources

---
+++ axes.v.glsl
@@ -0,0 +1,25 @@
+#version 120
+
+uniform vec3 sun;
+uniform float ambient;
+uniform float diffuse;
+uniform vec3[3] colors;
+
+attribute vec3 vertex;
+attribute vec3 normal;
+attribute int color_no;
+
+varying vec3 color;
+
+vec3 unit(vec3 v) {
+
+	return v / length(v);
+}
+
+void main(void) {
+
+	gl_Position = vec4(vertex, 1.0);
+
+	color = colors[color_no];
+	color *= length(sun) * (ambient + dot(unit(sun), unit(normal)) * diffuse);
+}
