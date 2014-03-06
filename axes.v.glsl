d7b8092b4e0ae06e8f46bdad7046b11875b78e0b
Karol Marcjan <karol.marcjan@gmail.com>
2014-03-06 18:54:44 +0100

    Move lighting calculations out of vertex shader

--- axes.v.glsl
+++ axes.v.glsl
@@ -9,17 +9,14 @@ attribute vec3 position;
 attribute vec3 normal;
 attribute int color_no;
 
+varying vec3 local_normal;
 varying vec3 color;
-
-vec3 unit(vec3 v) {
-
-	return v / length(v);
-}
 
 void main(void) {
 
 	gl_Position = vec4(position, 1.0);
 
+	local_normal = normal;
+
 	color = colors[color_no];
-	color *= length(sun) * (ambient + dot(unit(sun), unit(normal)) * diffuse);
 }
