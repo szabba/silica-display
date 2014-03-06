0f95467128f14dec3c1dc20a2af465acb80473f0
Karol Marcjan <karol.marcjan@gmail.com>
2014-03-06 18:55:38 +0100

    Calculate Phong lighting in Axes fragment shader
    (Specular is harwired to be 0)

--- axes.f.glsl
+++ axes.f.glsl
@@ -1,8 +1,26 @@
 #version 130
 
+uniform vec3 sun;
+uniform float ambient;
+uniform float diffuse;
+
+varying vec3 local_normal;
 varying vec3 color;
 
 void main(void) {
 
-	gl_FragColor = vec4(color, 1.0);
+	float I_0 = length(sun);
+
+	vec3 sun_dir = normalize(sun);
+	vec3 normal = normalize(local_normal);
+
+	float I = I_0 * ambient;
+
+	if (dot(sun_dir, normal) > 0) {
+		I += I_0 * dot(sun_dir, normal) * diffuse;
+	}
+
+	gl_FragColor.a = 1;
+
+	gl_FragColor.rgb = I * color;
 }
