ba1667b27243274261944d485a46d340136a1b9f
Karol Marcjan <karol.marcjan@gmail.com>
2014-03-07 12:54:12 +0100

    Add Cam.matrix

--- cam.py
+++ cam.py
@@ -3,9 +3,19 @@
 __all__ = ['Cam']
 
 
+import numpy
+
+
 class Cam(object):
     """The camera"""
 
     def __init__(self, config):
 
         self.__config = config
+
+        self.__matrix = numpy.eye(4)
+
+    def matrix(self):
+        """C.matric() -> camera matrix"""
+
+        return self.__matrix
