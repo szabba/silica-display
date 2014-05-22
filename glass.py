40a57c009130131d792408d911b034e139cd4829
Karol Marcjan <karol.marcjan@gmail.com>
2014-05-22 16:19:03 +0200

    Add glass.guess_size

--- glass.py
+++ glass.py
@@ -3,6 +3,29 @@
 __all__ = ['Glass']
 
 
+import re
+
+
+def guess_size(filename):
+    """guess_size(filename) -> widht, height, depth
+
+    Guess the size of a grid based on the name of the file that describes it.
+    """
+
+    match = re.match(
+            r'data(?P<w>\d+)x(?P<h>\d+)x(?P<d>\d+)t\d+_\d+.dat',
+            filename)
+
+    if match is None:
+        raise ValueError(
+                'Size of grid in \'%s\' cannot be guessed.' % filename)
+
+    return (
+            int(match.groupdict()['w']),
+            int(match.groupdict()['h']),
+            int(match.groupdict()['d']))
+
+
 class Glass(object):
     """The glass (or it's visible part)"""
 
