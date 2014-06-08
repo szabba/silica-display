5843d05451a019be336d24df1113edf75de3f32b
Karol Marcjan <karol.marcjan@gmail.com>
2014-06-08 14:33:22 +0200

    Implement partial Phong lighting in the glass shader

--- glass.f.glsl
+++ glass.f.glsl
@@ -3,9 +3,19 @@
 uniform vec3 color = vec3(1, 0, 0);
 uniform vec3 sun;
 
+uniform float ambient = 0.3;
+uniform float diffuse = 0.7;
+
 varying vec3 f_normal;
 
 void main(void) {
 
-	gl_FragColor = vec4(color * dot(normalize(sun), f_normal), 1);
+	float diffuse_product = dot(normalize(sun), f_normal);
+	diffuse_product *= float(diffuse_product > 0);
+
+	float intensity =
+		ambient +
+		diffuse * diffuse_product;
+
+	gl_FragColor = vec4(color * intensity, 1);
 }
