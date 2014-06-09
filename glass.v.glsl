b1ec50e9d74c2ebfa7bafb53db5f8cdc2c777d49
Karol Marcjan <karol.marcjan@gmail.com>
2014-06-09 12:13:14 +0200

    Pass position along to the fragment shader in glass.v.glsl

--- glass.v.glsl
+++ glass.v.glsl
@@ -6,10 +6,12 @@ attribute vec3 position;
 attribute vec3 normal;
 
 varying vec3 f_normal;
+varying vec3 f_position;
 
 void main(void) {
 
 	gl_Position = camera * vec4(position, 1.0);
 
 	f_normal = normal;
+	f_position = position;
 }
