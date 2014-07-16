ec1a11ab4e74864645270b411ab12fe0605e82c9
Karol Marcjan <karol.marcjan@gmail.com>
2014-07-16 18:25:53 +0200

    Add particles.Particles.__prepare_model

--- particles.py
+++ particles.py
@@ -2,6 +2,8 @@
 
 __all__ = ['Particles']
 
+import numpy
+
 
 class Particles(object):
     """The glass (or it's visible part)"""
@@ -10,3 +12,27 @@ class Particles(object):
 
         self.__config = config
         self.__cam = cam
+
+        self.__model = self.__prepare_model()
+
+    def __prepare_model(self):
+        """P.__prepare_model() -> (poses, normals, colors, vertex_count)"""
+
+        poses = numpy.array([
+            [1, 0, 0],
+            [0, 1, 0],
+            [0, -1, 0]])
+
+        normals = numpy.array([
+            [0, 0, 1],
+            [0, 0, 1],
+            [0, 0, 1]])
+
+        colors = numpy.array([
+            [1, 0, 0],
+            [0, 0, 1],
+            [0, 0, 1]])
+
+        vertex_count = 3
+
+        return poses, normals, colors, vertex_count
