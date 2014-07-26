ea9ba60dd9c63646c01d9c5ddd21a4243bea605c
Karol Marcjan <karol.marcjan@gmail.com>
2014-07-26 20:01:28 +0200

    Fix glass centering

--- camera.py
+++ camera.py
@@ -40,7 +40,7 @@ class Camera(object):
         Initial translation vector, in homogeneous coordinates, as a column matrix.
         """
 
-        return -numpy.array(
+        return numpy.array(
                 self.center_point() + (1,),
                 dtype=numpy.float
                 ).reshape((4, 1))
@@ -54,7 +54,7 @@ class Camera(object):
         dimmensions = self.__config.grid_size()
         repetitions = self.__config.glass_repetitions()
 
-        return tuple((dim * rep) / 2.0 for dim, rep in zip(dimmensions, repetitions))
+        return tuple(-(dim * rep) / 2.0 for dim, rep in zip(dimmensions, repetitions))
 
     def tick(self, dt):
         """C.tick(dt)
