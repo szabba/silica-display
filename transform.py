284a955e1e3f3d5bd45ddf8769883a2f48613e9c
Karol Marcjan <karol.marcjan@gmail.com>
2014-07-27 19:39:04 +0200

    Add transform.Transform.dirty

--- transform.py
+++ transform.py
@@ -11,3 +11,15 @@ class Transform(object):
         self.__used_by = set()
         self.__matrix = None
         self.__gl_matrix = None
+
+    def dirty(self):
+        """T.dirty()
+
+        Marks the transform as dirty, so that it will be recalculated next time
+        the matrix is requested.
+        """
+
+        self.__matrix, self.__gl_matrix = None, None
+
+        for user in self.__used_by:
+            user.dirty()
