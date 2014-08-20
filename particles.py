8fbba03106f8c15288f0ee67c9624242014b1dcf
Karol Marcjan <karol.marcjan@gmail.com>
2014-08-20 12:52:58 +0200

    Add particles.ParticleModel

--- particles.py
+++ particles.py
@@ -9,6 +9,29 @@ import numpy
 import shaders
 
 
+class ParticleModel(object):
+    """The 3D model for a single particle"""
+
+    def __init__(self):
+
+        self.__poses = numpy.array([
+            [1, 0, 0],
+            [0, 1, 0],
+            [0, -1, 0]])
+
+        self.__normals = numpy.array([
+            [0, 0, 1],
+            [0, 0, 1],
+            [0, 0, 1]])
+
+        self.__colors = numpy.array([
+            [1, 0, 0],
+            [0, 0, 1],
+            [0, 0, 1]])
+
+        self.__vertex_count = 3
+
+
 class Particles(object):
     """The glass (or it's visible part)"""
 
