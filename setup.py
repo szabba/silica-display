19bf827f967e8d49af69a048b7eb977e640765f6
Karol Marcjan <karol.marcjan@gmail.com>
2014-09-23 17:44:13 +0200

    Move src -> silica

--- setup.py
+++ setup.py
@@ -16,7 +16,6 @@ setup(
             'numpy>=1.8',
             'scipy==0.14'],
 
-        package_dir={'silica': 'src'},
         packages=find_packages(),
         package_data={
             'silica.viz.glass': ['*.glsl', 'glass_inline.c']})
