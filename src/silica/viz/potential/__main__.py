948b25dbe241b9058037fe2e91670032d14af225
Karol Marcjan <karol.marcjan@gmail.com>
2014-09-26 14:17:45 +0200

    Minimal working window in silica.viz.potential

---
+++ src/silica/viz/potential/__main__.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+
+import os.path
+import math
+import logging
+
+import pyglet
+from pyglet import gl
+
+from silica.viz.common import shaders
+from silica.viz.common import transform
+from silica.viz.common.transform.dicts import common_transforms
+from silica.viz.common.constants import *
+from silica.viz.common.camera import Cameraman, cam_transforms
+from silica.viz.common.axes import Axes
+from silica.viz.glass.config import Config
+
+
+class DisplayApp(object):
+    """Main object"""
+
+    def __init__(self, config):
+
+        self.__window = config.create_window()
+        keys = pyglet.window.key.KeyStateHandler()
+        transforms = {}
+
+        common_transforms(transforms, config, self.__window)
+        cam_transforms(transforms, config)
+
+        cam = transforms['camera']
+
+        self.__window.push_handlers(
+                Axes(config, transforms, self.__window))
+
+        self.__window.push_handlers(
+                Cameraman(config, keys, transforms))
+
+        self.__window.push_handlers(
+                keys)
+
+        pyglet.clock.set_fps_limit(config.max_fps())
+
+        self.__config = config
+
+    def run(self):
+        """DA.run()
+
+        Runs the app.
+        """
+
+        if not gl.gl_info.have_version(2, 1):
+
+            raise RuntimeError('OpenGL 2.1 required!')
+
+        gl.glEnable(gl.GL_DEPTH_TEST)
+
+        pyglet.app.run()
+
+
+if __name__ == '__main__':
+
+    shaders.shader_path.append(os.path.dirname(__file__))
+
+    app = DisplayApp(Config())
+    app.run()
