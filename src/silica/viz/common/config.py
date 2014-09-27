943cf204d46a15c3ccc3ecc9f2b598bf870f8359
Karol Marcjan <karol.marcjan@gmail.com>
2014-09-27 15:31:00 +0200

    Add silica.viz.common.config.SlicedGridArgsParser

--- src/silica/viz/common/config.py
+++ src/silica/viz/common/config.py
@@ -18,3 +18,29 @@ class BaseArgsParser(argparse.ArgumentParser):
                 '-F', '--fps', '--frame-rate',
                 help='number of particle animation frames to play per second',
                 type=float, default=3.0)
+
+
+class SlicedGridArgsParser(BaseArgsParser):
+    """An argument parser for things that display cubical grids that may be
+    sliced.
+    """
+
+    def __init__(self):
+
+        super(SlicedGridArgsParser, self).__init__()
+
+        self.add_argument(
+                '-s', '--slice',
+                help=''.join([
+                    'slice of ', self.object_sliced(), 'to display; described',
+                    ' by enclosed cube index ranges along all the axes']),
+                nargs=6, type=int,
+                default=(None, ) * 6)
+
+    def object_sliced(self):
+        """SGAP.object_sliced() -> string
+
+        Name of the object being sliced.
+        """
+
+        raise NotImplementedError(self.__class__.__name__, self.object_sliced.__name__)
