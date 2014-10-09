e8ab8577c0710e065e4311813fee45f460a6157a
Karol Marcjan <karol.marcjan@gmail.com>
2014-10-09 12:28:43 +0200

    Bump minor version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 setup(
         name='silica',
         url='bitbucket.org/szabba/silica_display',
-        version='0.1',
+        version='0.2',
 
         author='Karol Marcjan',
         author_email='karol.marcjan@gmail.com',
