afa26145a60b65fe1dad77aac819f63611677f9a
Karol Marcjan <karol.marcjan@gmail.com>
2014-10-27 00:37:15 +0100

    Add Vector.__add__

--- src/silica/viz/common/vectors.py
+++ src/silica/viz/common/vectors.py
@@ -7,3 +7,12 @@ class Vector(object):
     def __init__(self, x, y, z):
 
         self.x, self.y, self.z = x, y, z
+
+    def __add__(self, o):
+
+        if not isinstance(o, self.__class__.__name__):
+            raise TypeError(
+                    '%s should be a %s; vectors only add to vectors',
+                    o, self.__class__.__name__)
+
+        return Vector(self.x + o.x, self.y + o.y, self.z + o.z)
